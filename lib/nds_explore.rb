3f72be64bcd1d5b48dd99eb90ae0a1254d0d7daa
Madeline Corman <madelinecorman@gmail.com>
2019-11-08 18:43:07 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,14 +11,14 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-
+require 'pry'
 require 'pp'
 pp directors_database
   
 #print_first_directors_movie_titles = directors_database[0][:movies]
 #print_first_directors_movie_titles[:title]
 
-row_index = 0 
+#row_index = 0 
 #while row_index < directors_database.length do
  # binding.pry 
  #row_index += 1 
